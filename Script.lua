594e493db9cd9c25351a0f29174243ca0505571c
Natthawat69 <80118554+Natthawat69@users.noreply.github.com>
2021-04-20 18:17:43 +0700

    Update Script.lua

--- Script.lua
+++ Script.lua
@@ -1,12 +1,18 @@
-if game.PlaceId==394506555 then
-    loadstring(game:HttpGet("https://pastebin.com/raw/22cYTmNY",true))()
-    end
-if game.PlaceId==3956818381 then
-        loadstring(game:HttpGet("https://pastebin.com/raw/fKJ13JTn",true))()
-    end
-if game.PlaceId==6186867282 then
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/Natthawat69/ProjectXL-Natthawat/main/Project%20XL.lua",true))()
+if game.PlaceId == 2753915549 then -- Bloxfruit old
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/Natthawat69/BloxFruit-/main/Bloxfruit.lua",true))()
+else
+if game.PlaceId == 4442272183 then -- Bloxfruit new
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/Natthawat69/BloxFruit-/main/Bloxfruit.lua",true))()
+else
+if game.PlaceId == 6186867282 then -- ProjectXL
+   oadstring(game:HttpGet("https://raw.githubusercontent.com/Natthawat69/ProjectXL-Natthawat/main/Project%20XL.lua",true))()
+else
+if game.PlaceId == 3101667897 then -- Legends Of Speed 
+   loadstring(game:HttpGet("https://raw.githubusercontent.com/Natthawat69/Legends-Of-Speed/main/XD",true))()
+else
+if game.PlaceId == 6703932363 then -- Maokuma Piece
+   loadstring(game:HttpGet("https://raw.githubusercontent.com/Natthawat69/Maokuma-Piece/main/MaokumaPiece.lua",true))()
+else
+    game.Players.LocalPlayer:Kick("EvilHub not Support!!")
 end
-if game.PlaceId==6326691606 then
-    loadstring(game:HttpGet("hee",true))()
 end
